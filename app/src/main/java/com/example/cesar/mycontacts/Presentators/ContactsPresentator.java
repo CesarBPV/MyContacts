bdbd066dd5daf6f7ee235a4e58d6d775bd16a75f
Cesar <cesarpareja@upeu.edu.pe>
2018-05-30 12:23:04 -0500

    :)

---
+++ app/src/main/java/com/example/cesar/mycontacts/Presentators/ContactsPresentator.java
@@ -0,0 +1,6 @@
+package com.example.cesar.mycontacts.Presentators;
+
+import com.example.cesar.mycontacts.interfaces.ContactsInterface;
+
+public class ContactsPresentator implements ContactsInterface.Presenter {
+}
---
+++ app/src/main/java/com/example/cesar/mycontacts/interfaces/ContactsInterface.java
@@ -0,0 +1,15 @@
+package com.example.cesar.mycontacts.interfaces;
+
+public interface ContactsInterface {
+
+    interface View{
+    }
+
+    interface Presenter{
+    }
+
+    interface Model{
+
+    }
+
+}
